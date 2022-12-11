4e016a767ad57342b41a9a722015ef2f5763d179
smsiam-web <76879463+smsiam-web@users.noreply.github.com>
2022-12-11 17:03:59 +0600

    project setup

---
+++ app/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-white">
+      <div className="container">Header</div>
+    </header>
+  );
+};
+
+export default Header;
